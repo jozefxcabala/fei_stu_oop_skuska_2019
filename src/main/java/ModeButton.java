1f6a2840dcafc76efce91fefcda2012151809c98
jozefxcabala <xcabala@stuba.sk>
2021-06-02 14:21:41 +0200

    adding ModeButton - design and funcionality

---
+++ src/main/java/ModeButton.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ModeButton extends Button implements ActionListener {
+    private boolean drawing;
+
+    public ModeButton(String nameOfButton){
+        super(nameOfButton);
+        this.addActionListener(this);
+
+        this.drawing = true;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(this.drawing){
+            this.drawing = false;
+            System.out.println("MODE MARKING");
+        }
+        else{
+            this.drawing = true;
+            System.out.println("MODE DRAWING");
+        }
+    }
+}
--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -4,6 +4,7 @@ import java.awt.*;
 public class MyFrame extends JFrame {
     private ControlPanel controlPanel;
     private MyCanvas canvas;
+    private ModeButton modeButton;
 
     public MyFrame(String nameOfWindow){
         super(nameOfWindow);
@@ -14,6 +15,9 @@ public class MyFrame extends JFrame {
 
         this.canvas = new MyCanvas();
         this.add(this.canvas, BorderLayout.CENTER);
+
+        this.modeButton = new ModeButton("MODE");
+        this.add(this.modeButton, BorderLayout.PAGE_END);
 
         this.pack();
         this.setVisible(true);
