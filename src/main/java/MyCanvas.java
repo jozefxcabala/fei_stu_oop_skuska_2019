090efaae29dceaf5722089557c7ea36db18b0fa2
jozefxcabala <xcabala@stuba.sk>
2021-06-02 14:17:08 +0200

    Canvas panel - design

---
+++ src/main/java/MyCanvas.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyCanvas extends JPanel {
+    public MyCanvas(){
+        this.setPreferredSize(new Dimension(400,400));
+        this.setBackground(Color.WHITE);
+
+        this.setVisible(true);
+    }
+}
--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -3,12 +3,17 @@ import java.awt.*;
 
 public class MyFrame extends JFrame {
     private ControlPanel controlPanel;
+    private MyCanvas canvas;
+
     public MyFrame(String nameOfWindow){
         super(nameOfWindow);
         this.setLayout(new BorderLayout());
 
         this.controlPanel = new ControlPanel();
         this.add(this.controlPanel, BorderLayout.PAGE_START);
+
+        this.canvas = new MyCanvas();
+        this.add(this.canvas, BorderLayout.CENTER);
 
         this.pack();
         this.setVisible(true);
